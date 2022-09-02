7bbc23f00d9f26380d15cf16eab8104d89127a72
liguo-a <64149477+liguo-a@users.noreply.github.com>
2022-09-02 23:11:58 +0800

    Update Test.java
    pull commit

--- src/main/java/com/liguo/Test.java
+++ src/main/java/com/liguo/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("master test!");
         System.out.println("hell hot-fix test");
         System.out.println("hell push test");
+        System.out.println("hell pull test");
     }
 }
