ef2dd9d4a7b270f51cb8c9ca849f4b237f15ddbe
kevin <2387809188@qq.com>
2022-09-02 22:50:49 +0800

    hot-fix commit2

--- src/main/java/com/liguo/Test.java
+++ src/main/java/com/liguo/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("cesi");
         System.out.println("1111");
         System.out.println("hell hot-fix");
+        System.out.println("hell hot-fix test");
     }
 }
